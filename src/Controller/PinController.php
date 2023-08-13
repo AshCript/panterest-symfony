1c535e98f3ca80dfe2fa716088bdac0385fd6d9e
AshCript <asjosvah@gmail.com>
2023-08-13 16:43:38 +0300

    Creating show action for showing a specific pin by its ID

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -15,4 +15,14 @@ class PinController extends AbstractController
         $pins = $pinRepository->findAll();
         return $this->render('pin/index.html.twig', compact('pins'));
     }
+
+    #[Route('/pin/{id<[0-9]+>}', name: 'app_pin_show')]
+    public function show(int $id, PinRepository $pinRepository): Response
+    {
+        $pin = $pinRepository->find($id);
+        if(!$pin){
+            throw $this->createNotFoundException("Pin with id $id doesn't exist.");
+        }
+        return $this->render('pin/show.html.twig', compact('pin'));
+    }
 }
