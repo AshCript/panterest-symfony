396f983c5aeeb3db0d23456224222403d30e07d4
AshCript <asjosvah@gmail.com>
2023-08-21 16:47:25 +0300

    Pin Controller : Adding symfony validation

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -6,6 +6,7 @@ use App\Entity\Traits\Timestampable;
 use App\Repository\PinRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PinRepository::class)]
 #[ORM\Table(name: 'pins')]
@@ -20,9 +21,13 @@ class Pin
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Title can\'t be blank.')]
+    #[Assert\Length(min: 3, minMessage: 'Title must have at least 3 characters')]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(message: 'Description can\'t be blank.')]
+    #[Assert\Length(min: 10, minMessage: 'Description must have at least 10 characters')]
     private ?string $description = null;
 
     public function getId(): ?int
